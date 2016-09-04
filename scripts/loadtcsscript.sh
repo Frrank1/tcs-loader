9b686f05605f879f1420152052d43071df87d3b1
mhikeo <mhikeo@gmail.com>
2016-09-04 23:34:32 +0800

    Update scripts/loadtcsscript.sh

--- scripts/loadtcsscript.sh
+++ scripts/loadtcsscript.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CP=$SCRIPT_DIR/../target/tcs-loader-1.0.0.jar
 
-java -cp $CP com.topcoder.shared.util.dwload.TCLoadUtility -xmlfile $SCRIPT_DIR/load.xml
+java -cp $CP com.topcoder.shared.util.dwload.TCLoadUtility -xmlfile $SCRIPT_DIR/loadtcs.xml
 
 # java -cp $CP com.topcoder.shared.util.dwload.CacheClearer
